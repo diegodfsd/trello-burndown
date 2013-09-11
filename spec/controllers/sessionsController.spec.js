040b2dcea9381d8100774ac78bf9692d1ec9bb34
Diego Dias <diego.dfsd@gmail.com>
2013-09-10 23:22:29 -0300

    refactoring jasmine initialization

--- spec/controllers/sessionsController.spec.js
+++ spec/controllers/sessionsController.spec.js
@@ -1,13 +1,14 @@
 require(__dirname + '../../support/spec_helper');
 
 var Browser = require("zombie"),
+	config = require('./app/config/configurations')(),
 	browser;
 
  
 describe("sign in", function() {
 	
 	beforeEach(function () {
-		browser = new Browser({site: "http://localhost:3000"});
+		browser = new Browser({site: "http://localhost:" +  config.port });
 	});
 	
  	it("when is not authenticated should be redirect to signin page", function (next) {
