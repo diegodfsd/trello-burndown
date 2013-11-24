f9708af36bca9db2588d01838cd3b3868e01c133
Diego Dias <diego.dfsd@gmail.com>
2013-11-24 02:56:06 -0200

    removed console.log call

--- app/helpers/dynamicHelpers.js
+++ app/helpers/dynamicHelpers.js
@@ -2,7 +2,6 @@
 	exports.registerAll = function(app)
 	{
 		app.use(function(req, res, next) {
-			console.log( JSON.stringify(req.app.routes) );
 		    res.locals.flash = function(name) { return req.flash(name); };
 		    next();
 		});
