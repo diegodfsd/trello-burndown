45b62606f9dd0bce818eb622b2c4af6c352bb5bb
Diego Dias <diego.dfsd@gmail.com>
2013-11-24 02:55:02 -0200

    removed flash message no used

--- app/controllers/dashboardController.js
+++ app/controllers/dashboardController.js
@@ -2,10 +2,9 @@
 	var self = this,
 		config = require(__dirname + '../../config/configurations')();
 
-
 	// GET: Index
 	exports.index = function(req, res, next){
-		req.flash('success', 'Hello there!');
+		// req.flash('success', 'Hello there!');
 		res.render('index', { message: 'say hello to express mvc' });
 	};	
 })();
